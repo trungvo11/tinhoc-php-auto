ba138dce7a172dca2adb189e16156831ad82f571
root <root@ip-172-16-1-165.ap-southeast-1.compute.internal>
2021-02-03 04:30:32 +0000

    fix code stafftest.php

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "AAA";
-        $this->assertEquals('AAA', $staff->getFullName());
+        $staff->first_name = Doanh";
+        $this->assertEquals('Doanh', $staff->getFullName());
     }
 }
