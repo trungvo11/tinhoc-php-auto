b4ca83a9c81731c74424402be153fee83883af83
Trungvo11 <vttrung1710@gmail.com>
2021-02-03 05:42:32 +0000

    fix doanh

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Doanh4";
-        $this->assertEquals('Doanh4', $staff->getFullName());
+        $staff->first_name = "nguyen";
+        $this->assertEquals('Doanh', $staff->getFullName());
     }
 }
